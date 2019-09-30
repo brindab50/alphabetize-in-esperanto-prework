44de664f2fdb85c2e22ff791de0999b2ae362263
brinda behal <brindab50@gmail.com>
2019-09-30 21:58:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,11 +5,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |element|
     arr.split("").map do |letter|
-      
-    
- 
- 
-  
+     ESPERANTO_ALPHABET.index(letter)
+    end
   #binding.pry  
   end
-  end
+end
